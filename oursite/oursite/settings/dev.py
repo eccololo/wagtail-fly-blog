65fcf8e2b047abe4261dc89ae8f0e9dafde29486
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-07-04 20:58:27 +0200

    first commit

---
+++ oursite/oursite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!7_ki2ke&7z5&q9%)n#1f9226-arwqq%@&p3&!869e)!$+dysd"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
